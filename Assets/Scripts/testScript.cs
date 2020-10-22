bb9f2ecac617e596b399432982517971d9c2ebcf
硫硝酸酸雨 <60508733+sanseiuP@users.noreply.github.com>
2020-10-22 17:18:07 +0800

    test

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    int count = 50;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count --;
+        if (count == 0) {
+            GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+            count = 50;
+            }
+    }
+}
