7fdff9df61c9a03ad2f9202f81b873ed36265409
AlwaysConfident <62639467+AlwaysConfident@users.noreply.github.com>
2020-10-22 19:02:25 +0800

    Actor与Attack类相关

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    private string name;
+    private int HP;
+    private float speed;
+    private Transform transform;
+    Attack attack;
+
+    public void setMove(float direction)//设置移动
+    {
+
+    }
+
+    public void setAction(int action)//设置动作
+    {
+
+    }
+
+    private void encounterAttack(Attack attack)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
