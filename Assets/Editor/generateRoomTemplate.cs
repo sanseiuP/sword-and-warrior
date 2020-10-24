c926475bd3155dbd54348cb1d5b9629062d1dd66
硫硝酸酸雨 <60508733+sanseiuP@users.noreply.github.com>
2020-10-24 23:14:30 +0800

    场景

---
+++ Assets/Editor/generateRoomTemplate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateRoomTemplate
+{
+    [MenuItem("GameObject/CustomGenerate/RoomTemplate",false,10)]
+    public static void generateRoomTemplate(MenuCommand menuCommand) {
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>
+        ("Assets/Prefabs/Rooms/RoomTemplate.prefab");
+        GameObject go = GameObject.Instantiate<GameObject>(prefab);
+        GameObjectUtility.SetParentAndAlign(go, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+        Selection.activeObject = go;
+	}
+}
---
+++ Assets/Scripts/roomEdit/RoomData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+//记录房间的信息
+struct RoomInfo {
+    Vector2Int size; //房间的大小，用width*height表示、
+    //三层Tilemap
+    Tilemap ground;
+    Tilemap groundDetails;
+    Tilemap onTheGround;
+    //出口信息，四个方向
+    Vector2Int [] topExits;
+    Vector2Int [] leftExits;
+    Vector2Int [] rightExits;
+    Vector2Int [] BottomExits;
+}
+
+public class RoomData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
