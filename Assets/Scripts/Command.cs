d0adbcf2b05d116767c2faa9868211920ae2d5bf
1963306815@qq.com <1963306815@qq.com>
2020-11-24 14:38:28 +0800

    command, interactentity, coin, crystal

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 //暂时将所有函数都设置为public void
 public class Command : MonoBehaviour
 {
+    GameObject[] obj;
+
+    void Awake() //保证obj在Command生成时就被初始化
+    {
+        obj = FindObjectsOfType(typeof(GameObject)) as GameObject[]; //获取所有gameobject元素给数组obj
+    }
    
     public void move(Actor player) //不用传进移动方向direction，由getMoveInput()函数逐帧获取move_direction即可移动
     {
@@ -51,18 +57,62 @@ public class Command : MonoBehaviour
 
     public void roll(float direction)
     {
-
+        //what is roll? The roll of the warrior?
     }
 
     public void Attack(Actor player)
     {
-
+        float min_distance=-1.0;
+        Enemy e=null;
+        foreach (GameObject child in obj) //遍历所有gameobject
+        {
+            //Debug.Log(child.gameObject.name); //可以在unity控制台测试一下是否成功获取所有元素
+            if (child.gameObject.tag == "Enemy") //如果是Enemy，就计算和player的距离
+            {
+                float distance;
+                Vector2 temp;
+                temp.x=child.gameObject.rigidbody.position.x-player.rigidbody.position.x;
+                temp.y=child.gameObject.rigidbody.position.y-player.rigidbody.position.y;
+                distance=Math.Sqrt(temp.x*temp.x+temp.y*temp.y);
+                if (e==null||distance<min_distance) {
+                    e=child.gameObject;
+                    min_distance=distance;
+                }
+                Destroy(child.gameObject);
+            }
+        }
+        Vector2 direction;
+        direction.x=e.rigidbody.position.x-player.rigidbody.position.x;
+        direction.y=e.rigidbody.position.y-player.rigidbody.position.y;
+        player.attack.Shoot(direction);
+        player.attack.OnCollisionEnter2D(e);
     }
 
-    public void switchWeapon()
+    public void switchWeapon(Warrior w, float threshold=0.5) //threshold为捡枪最小距离
     {
-
+        float min_distance=-1.0;
+        Item weapon=null;
+        foreach (GameObject child in obj) //遍历所有gameobject
+        {
+            if (child.gameObject.tag == "Item") //如果是Item，就计算和w的距离
+            {
+                float distance;
+                Vector2 temp;
+                temp.x=child.gameObject.rigidbody.position.x-w.rigidbody.position.x;
+                temp.y=child.gameObject.rigidbody.position.y-w.rigidbody.position.y;
+                distance=Math.Sqrt(temp.x*temp.x+temp.y*temp.y);
+                if (weapon==null||distance<min_distance) {
+                    weapon=child.gameObject;
+                    min_distance=distance;
+                }
+                Destroy(child.gameObject);
+            }
+        }
+        if (min_distance>=0&&min_distance<=threshold) {
+            w.setWeapon(weapon);
+        }
     }
+
     public void interact()
     {
         
--- Assets/Scripts/InteractEntity.cs
+++ Assets/Scripts/InteractEntity.cs
@@ -1,11 +1,24 @@
 using System;
 using UnityEngine;
 
+enum InteractEntityType
+{
+    Bullet=0,
+    Coin=1,
+    Crystal=2,
+    Gun=3
+};
+
 //邹凯韬、张志扬负责
 public class InteractEntity :MonoBehaviour
 {
-    public InteractEntity type;
+    public InteractEntityType type;
     public Item item;
     public int cost;
 
+    InteractEntity(InteractEntityType t, Item i, int c){
+        this.type=t;
+        this.item=i;
+        this.cost=c;
+    }
 }
