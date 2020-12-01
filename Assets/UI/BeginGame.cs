82565d7806322d5c877cb71de022b38ac80804d3
Leon <mpzleon@163.com>
2020-12-01 20:46:06 +0800

    abc

---
+++ Assets/UI/BeginGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BeginGame : MonoBehaviour
+{
+    public void beginGame()
+    {
+        SceneManager.LoadScene("RoomTest");
+    }
+}
